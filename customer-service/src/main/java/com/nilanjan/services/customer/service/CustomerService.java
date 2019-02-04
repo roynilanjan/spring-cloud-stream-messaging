fa2ac1270cac4ae5df3b2da2d6a1a86d1d1a849a
Roy <Nilanjan.Roy@emc.com>
2019-02-04 17:24:37 +0530

    update order-service

--- customer-service/src/main/java/com/nilanjan/services/customer/service/CustomerService.java
+++ customer-service/src/main/java/com/nilanjan/services/customer/service/CustomerService.java
@@ -36,14 +36,14 @@ public class CustomerService {
     public void process(final Order order) throws JsonProcessingException {
         LOGGER.info("Order processed: {}", mapper.writeValueAsString(order));
         List<Customer> customers =  customerRepository.findByCustomer(order.getCustomerId());
-        Customer account = customers.get(0);
-        LOGGER.info("Customer found: {}", mapper.writeValueAsString(account));
+        Customer customer = customers.get(0);
+        LOGGER.info("Customer found: {}", mapper.writeValueAsString(customer));
         List<Inventory> inventories = inventoryClient.findByIds(order.getProductIds());
         LOGGER.info("Products found: {}", mapper.writeValueAsString(inventories));
         inventories.forEach(p -> order.setPrice(order.getPrice() + p.getPrice()));
-        if (order.getPrice() <= account.getBalance()) {
+        if (order.getPrice() <= customer.getBalance()) {
             order.setStatus(OrderStatus.APPROVED);
-            account.setBalance(account.getBalance() - order.getPrice());
+            customer.setBalance(customer.getBalance() - order.getPrice());
         } else {
             order.setStatus(OrderStatus.REJECTED);
         }
